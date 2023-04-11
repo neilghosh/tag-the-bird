8d40c8bac9f27489ebd43fc9d22b373418fb01a5
Neil Ghosh <neilghosh@salesforce.com>
2023-04-11 23:04:39 +0530

    Fixed Lens response parsing

--- rest/get-identify-handler.js
+++ rest/get-identify-handler.js
@@ -3,7 +3,7 @@ const { v4: uuidv4 } = require("uuid");
 const fs = require("fs");
 const Stream = require("stream").Transform;
 const vision = require("@google-cloud/vision");
-const axios = require('axios');
+const axios = require("axios");
 
 function identify(photoId, access_token) {
   console.log("Identifying objects in " + photoId);
@@ -32,58 +32,76 @@ function identify(photoId, access_token) {
 const getIdentifyHandler = async (req, res) => {
   access_token = req.session.access_token;
   photoId = req.query.photoid;
-  if(req.query.detectobject === 'true') {
+  if (req.query.detectobject === "true") {
     let options = {
       headers: {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0'
+        "User-Agent":
+          "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0",
       },
       maxRedirects: 0,
       validateStatus: function (status) {
         // if this function returns true, exception is not thrown, so
         // in simplest case just return true to handle status checks externally.
         return true;
-      }
-    }
-    let result = await axios.get('https://lens.google.com/uploadbyurl?url='+photoId, options);
-    console.log(result);
-    const lensUrl = result.headers['location'];
+      },
+    };
+    let result = await axios.get(
+      "https://lens.google.com/uploadbyurl?url=" + photoId,
+      options
+    );
+    //console.log(result);
+    const lensUrl = result.headers["location"];
     result = await axios.get(lensUrl);
     const lensResult = result.data;
     //const regex = /dir="ltr" class.*?>(.*?)<\/div/gm;
     const regex = /data:(\[\[.*?https.*?"])/gm;
     const matches = lensResult.matchAll(regex);
-    let objects = []; 
+    let objects = [];
     for (const match of matches) {
       objects.push(match);
-      console.log(match);
-      console.log(match.index)
+      //console.log(match);
+      //console.log(match.index);
     }
-    const payload = match[0];
-    const json = JSON.parse(payload);
-    const values = json[2][3][0][0][2];
-    res.end(JSON.stringify(objects));
-  } 
-  const uuid = await identify(photoId, access_token);
-  //console.log(mediaItems.mediaItems.length);
-  console.log(uuid);
+    let values = [];
+    for(const object of objects){
+      try {
+        //TODO we dont know which one is well formatted json so taking a chance on every one.
+        values = JSON.parse(object[0].substring(5))[2][3][0][0][2];
+        continue;
+      } catch (error) {
+        console.log("Unable to parse ", object[0].substring(5));
+      }
+    }
 
-  // Creates a client
-  const client = new vision.ImageAnnotatorClient();
+    const labels = [];
+    for (const label of values) {
+      labels.push({ description: label[2] });
+    }
+    res.write(JSON.stringify(labels));
+    res.end();
+  } else {
+    const uuid = await identify(photoId, access_token);
+    //console.log(mediaItems.mediaItems.length);
+    console.log(uuid);
 
-  const fileName = "/tmp/" + uuid;
+    // Creates a client
+    const client = new vision.ImageAnnotatorClient();
 
-  // Performs label detection on the local file
-  const [result] = await client.labelDetection(fileName);
-  const labels = result.labelAnnotations;
-  console.log("Labels:");
-  labels.forEach((label) => console.log(label.description));
-  res.write(JSON.stringify(labels));
+    const fileName = "/tmp/" + uuid;
 
-  // res.render("photos",{
-  //   username: "neil",
-  //   mediaItems: mediaItems
-  // }); // index refers to index.ejs
-  res.end();
+    // Performs label detection on the local file
+    const [result] = await client.labelDetection(fileName);
+    const labels = result.labelAnnotations;
+    console.log("Labels:");
+    labels.forEach((label) => console.log(label.description));
+    res.write(JSON.stringify(labels));
+
+    // res.render("photos",{
+    //   username: "neil",
+    //   mediaItems: mediaItems
+    // }); // index refers to index.ejs
+    res.end();
+  }
 };
 
 module.exports = {
